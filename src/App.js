a850d3f10444d83511822c62d9bd27fdb4608ae5
WonBeomChoi <cwb2310@gmail.com>
2022-12-23 19:47:36 +0900

    리액트가 다시 안그려줘서 섭섭함..

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react"
 
 function AddCalculator() {
+  let num1 = 0;
+  let num2 = 0;
+  console.log("더하기 컴포넌트 함수가 실행되었습니다.")
   return(
     <div>
       <h2>더하기</h2>
-      <input type="number" name="num1" />
-      <input type="number" name="num2" />
-      결과 : 
+      <input type="number" name="num1" onChange={(e)=>{
+        num1=Number(e.target.value)
+        console.log(num1)
+        }}/>
+      <input type="number" name="num2" onChange={(e)=>{
+        num2=Number(e.target.value)
+        console.log(num2)
+
+      }}/>
+      결과 : {num1+num2}
     </div>
 
   )
