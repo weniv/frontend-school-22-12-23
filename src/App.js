a4eac9a1fe70546f5cd3792567bc4e84cf12654e
WonBeomChoi <cwb2310@gmail.com>
2022-12-23 20:44:19 +0900

    state끌어올리기

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React, { useEffect, useState } from "react"
 
-function AddCalculator() {
-  const [num1, setNum1] = useState(0)
-  const [num2, setNum2] = useState(0)
-  const handleNum1 = (e)=>{
-    const newNum1= parseInt(e.target.value)
-    setNum1(newNum1)
-  }
-  const handleNum2 = (e)=>{
-    const newNum2= parseInt(e.target.value)
-    setNum2(newNum2)
-  }
+function AddCalculator(props) {
   return(
     <div>
       <h2>더하기</h2>
       
-      <input type="number" name="num1" value={num1} onChange={handleNum1}/>
-      <input type="number" name="num2" value={num2} onChange={handleNum2}/>
-      결과 : {num1+num2}
+      <input type="number" name="num1" value={props.num1} onChange={props.handleNum1}/>
+      <input type="number" name="num2" value={props.num2} onChange={props.handleNum2}/>
+      결과 : {props.num1+props.num2}
     </div>
 
   )
 }
 
-function SubCalculator() {
+function SubCalculator(props) {
+  return(
+    <div>
+      <h2>빼기</h2>
+      <input type="number" name="num1" value={props.num1} onChange={props.handleNum1}/>
+      <input type="number" name="num2" value={props.num2} onChange={props.handleNum2}/>
+      결과 : {props.num1-props.num2}
+    </div>
+  )
+}
+
+function App() {
   const [num1, setNum1] = useState(0)
   const [num2, setNum2] = useState(0)
   const handleNum1 = (e)=>{
@@ -34,23 +35,11 @@ function SubCalculator() {
     const newNum2= parseInt(e.target.value)
     setNum2(newNum2)
   }
-  return(
-    <div>
-      <h2>빼기</h2>
-      <input type="number" name="num1" value={num1} onChange={handleNum1}/>
-      <input type="number" name="num2" value={num2} onChange={handleNum2}/>
-      결과 : {num1-num2}
-    </div>
-
-  )
-}
-
-function App() {
   return (
     <div>
       <h1>계산기</h1>
-      <AddCalculator/>
-      <SubCalculator/>
+      <AddCalculator num1={num1} num2={num2} handleNum1={handleNum1} handleNum2={handleNum2}/>
+      <SubCalculator num1={num1} num2={num2} handleNum1={handleNum1} handleNum2={handleNum2}/>
     </div>
   );
 }
