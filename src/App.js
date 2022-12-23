38f6bc0625d2aec90cc8bdb65a69130c458b7248
WonBeomChoi <cwb2310@gmail.com>
2022-12-23 21:05:46 +0900

    props 너무 귀찮음... redux라는걸 써보자

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function AddCalculator(props) {
   return(
     <div>
       <h2>더하기</h2>
-      
       <input type="number" name="num1" value={props.num1} onChange={props.handleNum1}/>
       <input type="number" name="num2" value={props.num2} onChange={props.handleNum2}/>
       결과 : {props.num1+props.num2}
@@ -24,12 +23,8 @@ function SubCalculator(props) {
   )
 }
 function App(){
-  return(
-    <Container/>
-  )
-}
 
-function Container() {
+
   const [num1, setNum1] = useState(0)
   const [num2, setNum2] = useState(0)
   const handleNum1 = (e)=>{
@@ -40,11 +35,17 @@ function Container() {
     const newNum2= parseInt(e.target.value)
     setNum2(newNum2)
   }
+  return(
+    <Container num1={num1} num2={num2} handleNum1={handleNum1} handleNum2={handleNum2}/>
+  )
+}
+
+function Container(props) {
   return (
     <div>
       <h1>계산기</h1>
-      <AddCalculator num1={num1} num2={num2} handleNum1={handleNum1} handleNum2={handleNum2}/>
-      <SubCalculator num1={num1} num2={num2} handleNum1={handleNum1} handleNum2={handleNum2}/>
+      <AddCalculator num1={props.num1} num2={props.num2} handleNum1={props.handleNum1} handleNum2={props.handleNum2}/>
+      <SubCalculator num1={props.num1} num2={props.num2} handleNum1={props.handleNum1} handleNum2={props.handleNum2}/>
     </div>
   );
 }
