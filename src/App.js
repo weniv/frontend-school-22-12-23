0e72f39e4eb8bd1e09cc23d8cf4a2253d9791692
WonBeomChoi <cwb2310@gmail.com>
2022-12-23 21:47:16 +0900

    리덕스 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,53 +1,96 @@
 import React, { useState } from "react"
+import { Provider, useDispatch, useSelector } from "react-redux"
+import { createStore } from "redux"
 
-function AddCalculator(props) {
-  return(
-    <div>
-      <h2>더하기</h2>
-      <input type="number" name="num1" value={props.num1} onChange={props.handleNum1}/>
-      <input type="number" name="num2" value={props.num2} onChange={props.handleNum2}/>
-      결과 : {props.num1+props.num2}
-    </div>
+const initialState ={num1:0, num2:0}
+const 은행원 = (상태, 행동)=>{
+  switch (행동.type) {
+    case "setNum1":
+      //어려운 로직들 막 작성하기~
+      const 결과값 = 행동.값
+      return {...상태, num1:결과값}//새로 업데이트될 상태를 return해준다!
+    case "setNum2":
+      return {...상태, num2:행동.값}
+    default:
+      return 상태
+      break;
+  }
+} 
+const 은행 = createStore(은행원, initialState)
 
-  )
+const 행동들 = {
+  setNum1(값){
+    return {type:"setNum1",값:값}
+  },
+  setNum2(값){
+    return {type:"setNum2",값:값}
+  }
 }
 
-function SubCalculator(props) {
-  return(
-    <div>
-      <h2>빼기</h2>
-      <input type="number" name="num1" value={props.num1} onChange={props.handleNum1}/>
-      <input type="number" name="num2" value={props.num2} onChange={props.handleNum2}/>
-      결과 : {props.num1-props.num2}
-    </div>
-  )
-}
 function App(){
-
-
-  const [num1, setNum1] = useState(0)
-  const [num2, setNum2] = useState(0)
-  const handleNum1 = (e)=>{
-    const newNum1= parseInt(e.target.value)
-    setNum1(newNum1)
-  }
-  const handleNum2 = (e)=>{
-    const newNum2= parseInt(e.target.value)
-    setNum2(newNum2)
-  }
   return(
-    <Container num1={num1} num2={num2} handleNum1={handleNum1} handleNum2={handleNum2}/>
+    <Provider store={은행}>
+      <Container />
+    </Provider>
   )
 }
 
-function Container(props) {
+function Container() {
   return (
     <div>
       <h1>계산기</h1>
-      <AddCalculator num1={props.num1} num2={props.num2} handleNum1={props.handleNum1} handleNum2={props.handleNum2}/>
-      <SubCalculator num1={props.num1} num2={props.num2} handleNum1={props.handleNum1} handleNum2={props.handleNum2}/>
+      <AddCalculator />
+      <SubCalculator />
     </div>
   );
+}
+
+
+function AddCalculator() {
+  const {num1, num2} = useSelector(상태=>상태)
+  const dispatch = useDispatch()
+  const {setNum1,setNum2} = 행동들
+  // const num1 = useSelector(상태=>상태.num1)
+  // const num2 = useSelector(상태=>상태.num2)
+  const handleNum1 = (e)=>{
+    const newNum1 = parseInt(e.target.value)
+    dispatch(setNum1(newNum1))
+  }
+  const handleNum2 = (e)=>{
+    const newNum2 = parseInt(e.target.value)
+    dispatch(setNum2(newNum2))
+  }
+  return(
+    <div>
+      <h2>더하기</h2>
+      <input type="number" name="num1" value={num1} onChange={handleNum1}/>
+      <input type="number" name="num2" value={num2} onChange={handleNum2}/>
+      결과 : {num1+num2}
+    </div>
+  )
+}
+
+function SubCalculator() {
+  const {num1, num2} = useSelector(상태=>상태)
+  const dispatch = useDispatch()
+  // const num1 = useSelector(상태=>상태.num1)
+  // const num2 = useSelector(상태=>상태.num2)
+  const handleNum1 = (e)=>{
+    const newNum1 = parseInt(e.target.value)
+    dispatch({type:"setNum1",값:newNum1})
+  }
+  const handleNum2 = (e)=>{
+    const newNum2 = parseInt(e.target.value)
+    dispatch()
+  }
+  return(
+    <div>
+      <h2>빼기</h2>
+      <input type="number" name="num1" value={num1} onChange={handleNum1}/>
+      <input type="number" name="num2" value={num2} onChange={handleNum2}/>
+      결과 : {num1-num2}
+    </div>
+  )
 }
 
 export default App;
